71ce8e324014d6fab5afd3b5ab85d52776c9b9dd
ArulMC123 <99807639+ArulMC123@users.noreply.github.com>
2022-05-15 22:20:38 +0700

    Update prank.py

--- prank.py
+++ prank.py
@@ -26,30 +26,30 @@ banner= """
 \033[37;1m ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝ \033[37;1m╚══════╝╚═╝     ╚═╝╚══════╝
 
 \033[33;1m╔════════════════════════════════════════════════╗
-\033[33;1m║  \033[36;1m [•] Authour : Asrul                      \033[33;1m ║
+\033[33;1m║  \033[36;1m [•] Authour : Asrul                         \033[33;1m ║
 \033[33;1m║  \033[36;1m [•] gitHub  : https:github.com/ArulMC123    \033[33;1m ║
-\033[33;1m║  \033[36;1m [•] Yotube  : Arul MINIKREP                      \033[33;1m ║
+\033[33;1m║  \033[36;1m [•] Yotube  : Arul MINIKREP                 \033[33;1m ║
 \033[33;1m╚════════════════════════════════════════════════╝
 \033[36;1m╔═══════════════════════════╗
-\033[36;1m║\033[33;1m GUNAKAN DENGAN BENAR \033[36;1m ║
+\033[36;1m║\033[33;1m GUNAKAN DENGAN BENAR    \033[36;1m ║
 \033[36;1m╚═══════════════════════════╝"""
 sleep(1)
 print(banner)
 
 # Jangan lu ubah ngab
-print ('\033[31;1m[!] \033[32;1mContoh nomor : \033[33;1m8xxxxxxx')
+print ('\033[31;1m[!] \033[32;1mExample \033[33;1m8xxxxxxx')
 
 nomor = input(' \033[36;1minput nomor lu :\033[33;1m ')
-print ('\033[32;1mMASUKAN JUMLAH SPAMNYA ??')
+print ('\033[32;1mMASUKAN JUMLAH SPAMNYA !!')
 print ('')
 jm = int(input('\033[31;1m Jumlah Spam :\033[33;1m '))
 
 for i in range(jm):
       req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor}).text
       if "mengirim" in req:
-            print ('\n \033[37;1m[\033[32;1m√\033[37;1m]\033[32;1mMENGIRIM SPAM')
+            print ('\n \033[37;1m[\033[32;1m✓\033[37;1m]\033[32;1mSUKSES MENGIRIM PESAN')
       else:
-            print ('\n \033[37;1m[\033[33;1m•••\033[37;1m]\033[36;1mMENCOBA MENGIRIM SPAM')
+            print ('\n \033[37;1m[\033[33;1mLOADING\033[37;1m]\033[36;1mSEDANG MENGIRIM PESAN')
 
 
 # subscribe MisterAM
